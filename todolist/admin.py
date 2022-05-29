a3f89419364baf0dae9f6e9af7f0cbfeaf5eef04
Alex <alx@mail.ru>
2022-05-29 17:40:41 +0300

    change admin for todolist

--- todolist/admin.py
+++ todolist/admin.py
@@ -6,5 +6,11 @@ from .models import Todolist
 
 @admin.register(Todolist)
 class TodoListAdmin(admin.ModelAdmin):
-    list_display = ('title', 'important', 'public', 'status')
+    list_display = ('title', 'important', 'public', 'status', 'create_at', 'update_at')
+
+    readonly_fields = ('create_at',)
+
+
+
+
 
